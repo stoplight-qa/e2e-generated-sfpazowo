c945d02a9bebe309192c1258e2e62961234bb46e
qa-sipaq-2-ycqn@example.com <qa-sipaq-2-ycqn@example.com>
2023-05-11 16:43:47 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16431.stoplight-dev.com/api/v1/projects/cHJqOjE0MDI/spectral.js?branch_slug=main&token=7364c10c-35d7-4ae2-831e-72b6fceb4ead";
+export default { extends: ruleset };
